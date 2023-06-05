20699587fe748bf62c00b6144ef0e0f28acaa217
Magdalina1 <majdalol95@gmail.com>
2023-06-05 08:25:24 -0700

    8

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(bytes.fromhex("6f626a6563742d6f7269656e7465642070726f6772616d6d696e67207769746820507974686f6e").decode())
