c8892b681eee0d1a19fc39f46ffd27e13cb87880
Magdalina1 <majdalol95@gmail.com>
2023-06-05 07:48:46 -0700

    fixing 6

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,7 @@
+#!/user/bin/python3
+word = "Holberton"
+word_first_3, *_ , word_last_2 = word
+middle_word = "".join(list(word)[2:-2])
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
