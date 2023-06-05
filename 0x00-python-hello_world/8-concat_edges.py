b31e94a980430bcdaea62254981a016a513526a6
Magdalina1 <majdalol95@gmail.com>
2023-06-05 08:35:15 -0700

    fixing 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(bytes.fromhex("6f626a6563742d6f7269656e7465642070726f6772616d6d696e67207769746820507974686f6e").decode())
+str1 = "object-oriented"
+str2 = " programming"
+str3 = " with"
+str4 = " Python"
+print("{}{}{}{}".format(str1, str2, str3, str4).strip())
