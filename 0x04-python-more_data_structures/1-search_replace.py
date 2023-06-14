18878f30a6884a969d6f5283e35ea92bedd7a467
Magdalina1 <majdalol95@gmail.com>
2023-06-14 05:21:02 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,9 +4,9 @@ def search_replace(my_list, search, replace):
 
     for itm in my_list:
         if itm == search:
-            new_list.oppend(replace)
+            new_list.append(replace)
 
         else:
-            new_list.oppend(itm)
+            new_list.append(itm)
 
     return new_list
