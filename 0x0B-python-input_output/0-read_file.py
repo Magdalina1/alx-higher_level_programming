3118eb9daa5112e88785771c7fb50717e08ab6ae
Magdalina1 <majdalol95@gmail.com>
2023-07-11 03:43:18 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,7 @@
 
 def read_file(filename=""):
     """Reads from filename and prints its contents to stdout."""
-    with open(filename) as file:
+
+    with open(filename) as f:
         read_text = f.read()
         print(read_text, end="")
