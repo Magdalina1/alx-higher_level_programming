3fdf9ac65648a432eb3f2f88c3f7022bfda97e76
Magdalina1 <majdalol95@gmail.com>
2023-07-16 07:13:02 -0700

    base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a Base class for other classes in the project."""
+
+
+import json
+import os
+import csv
+
+
+class Base:
+    """Private class attribute: __nb_objects."""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialization of a Base instance."""
+
+        if type(id) != int and id is not None:
+            raise TypeError("id most be integer")
+
+        if id is not None:
+            self.id = id
+
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
