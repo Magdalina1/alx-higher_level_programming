f858cbbb69e0c30a389a41df8d7f67bd200f879a
Magdalina1 <majdalol95@gmail.com>
2023-08-16 14:58:51 -0700

    fix 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -5,6 +5,6 @@
 -- If the database hbtn_0d_usa already exists, your script should not fail
 -- If the table states already exists, your script should not fail
 
-USE hbtn_0d_usa;
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.state (id INT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.state (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
