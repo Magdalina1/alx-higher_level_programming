99880afdb2c774a06466034b33e8f686ba5d7d12
Magdalina1 <majdalol95@gmail.com>
2023-08-16 13:55:47 -0700

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,6 +5,6 @@
 -- If the user user_0d_2 already exists, your script should not fail
 
 CREATE TABLE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2@localhost INDENTIFIED BY user_0d_2_pwd;
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' INDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
