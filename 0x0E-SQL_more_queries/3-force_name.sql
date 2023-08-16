e701b262528ba607b0f7dcf9a65a6474757f6604
Magdalina1 <majdalol95@gmail.com>
2023-08-16 14:15:52 -0700

    3-force_name.sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -5,4 +5,4 @@
 -- The database name will be passed as an argument of the mysql command
 -- If the table force_name already exists, your script should not fail
 
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(258), NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(258) NOT NULL);
