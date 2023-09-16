d606ec95421a6ff7fc93243afd817889bfd7678b
Magdalina1 <majdalol95@gmail.com>
2023-09-16 04:47:29 -0700

    100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-This script prints all City objects
-from the database `hbtn_0e_14_usa`.
-"""
+"""This script prints all City objects from the database `hbtn_0e_14_usa`."""
 
 from sys import argv
 from relationship_state import Base, State
