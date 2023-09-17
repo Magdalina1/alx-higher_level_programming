c892672c945e984d3234670034443265c7702a2e
Magdalina1 <majdalol95@gmail.com>
2023-09-17 11:48:39 -0700

    fixing relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,5 +1,6 @@
- #!/usr/bin/python3
-"""This script defines a State class and a Base class to work with MySQLAlchemy ORM."""
+#!/usr/bin/python3
+"""This script defines a State class and
+a Base class to work with MySQLAlchemy ORM."""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
